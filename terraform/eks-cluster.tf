5b8931a9e773df6ba7750881f9d976134cb3b169
raju0509 <rajuaws55@gmail.com>
2026-04-06 16:32:41 +0530

    i have testing3

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -4,24 +4,21 @@ module "eks" {
 
   cluster_name    = local.cluster_name
   cluster_version = "1.29"
-  
-  enable_kms_key = false
+
+  enable_kms_key            = false
   cluster_encryption_config = {}
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
-  
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
-
+      name           = "node-group-1"
       instance_types = ["t3.small"]
 
       min_size     = 1
@@ -30,8 +27,7 @@ module "eks" {
     }
 
     two = {
-      name = "node-group-2"
-
+      name           = "node-group-2"
       instance_types = ["t3.small"]
 
       min_size     = 1
